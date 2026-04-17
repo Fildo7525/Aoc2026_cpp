5e8e6e139e6089479e3536d371c57b41332ca2c4
Filip Lobpreis <f.lobpreis@gmail.com>
2026-04-17 23:47:18 +0200

    day2 pt 1
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

---
+++ Day2/main.cpp
@@ -0,0 +1,82 @@
+#include "Day2.h"
+#include <algorithm>
+#include <print>
+#include <string>
+
+using ull = unsigned long long;
+
+struct Range {
+	ull start;
+	ull end;
+};
+
+
+std::vector<std::string> split(const std::string &str, const char delimiter);
+bool isValidID(const std::string &id);
+
+int main(int argc, char *argv[])
+{
+	std::fstream input("../input", std::ios::in);
+	std::string line;
+
+	std::vector<Range> idRanges;
+
+	while (std::getline(input, line)) {
+		std::println("Splitting {}", line);
+		auto devided = split(line, ',');
+		for (auto &r : devided) {
+			auto rng = split(r, '-');
+			idRanges.push_back(Range{std::stoull(rng[0]), std::stoull(rng[1])});
+			std::println("Range[.start={}, .end={}]", idRanges.back().start, idRanges.back().end);
+		}
+	}
+
+	ull sum = 0;
+	for (const auto &r : idRanges) {
+		std::println("Chacking range {} - {}", r.start, r.end);
+		for (ull i = r.start; i <= r.end; i++) {
+			if (!isValidID(std::to_string(i))) {
+				sum += i;
+				std::println("Invalid number: {}", i);
+			}
+		}
+	}
+	std::println("SUM of invalid: {}", sum);
+
+	return 0;
+}
+
+std::vector<std::string> split(const std::string &str, const char delimiter)
+{
+	std::vector<std::string> out;
+	auto start = str.begin();
+	while (true) {
+		auto found = std::find(start, str.end(), delimiter);
+		if (found == str.end()) {
+			out.push_back(str.substr(start-str.begin()));
+			break;
+		}
+
+		out.push_back(str.substr(start - str.begin(), found - start));
+		start = found+1;
+	}
+
+	return out;
+}
+
+bool checkSequence(const std::string &id)
+{
+	const auto &a = id.substr(0, (id.size()/2));
+	const auto &b = id.substr(id.size()/2);
+	return a != b;
+}
+
+bool isValidID(const std::string &id)
+{
+	if (id[0] == 0) {
+		return false;
+	}
+
+	return checkSequence(id);
+}
+
---
+++ Day2/src/Day2.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <iterator>
+#include <memory>
+#include <numeric>
+#include <vector>
+
+class Day2
+{
+public:
+	Day2() = default;
+
+};
+
