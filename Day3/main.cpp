dbad8a52e06f74dbc19a5cddeaee0921798135df
Filip Lobpreis <f.lobpreis@gmail.com>
2026-04-19 12:22:45 +0200

    day3 pt 2
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- Day3/main.cpp
+++ Day3/main.cpp
@@ -5,28 +5,33 @@
 #include <cstdio>
 #include <ranges>
 
+constexpr int MAX_BATTERIES = 12;
+
 int main(int argc, char *argv[])
 {
 	std::fstream input("../input", std::ios::in);
 	std::string line;
-	int sum = 0;
+	unsigned long long sum = 0;
 
 	while (std::getline(input, line)) {
 		std::vector<uint8_t> batteries;
 		batteries.reserve(line.size());
 		std::ranges::for_each(line, [&batteries](const char c) { batteries.push_back(c - '0'); });
 
-		std::ranges::subrange start(batteries.begin(), batteries.end() - 1);
-		auto first = std::ranges::max_element(start);
-		// std::println("first range: {}", start);
+		std::vector<uint8_t>::iterator first = batteries.begin();
 
-		std::ranges::subrange end(first + 1, batteries.end());
-		// std::println("second range: {}", end);
+		unsigned long long mid = 0;
+		for (int i = MAX_BATTERIES-1; i >= 0; i--) {
+			std::ranges::subrange start(first, batteries.end() - i);
+			// std::println("range: {}", start);
+			first = std::ranges::max_element(start);
 
-		auto mid = *first * 10 + std::ranges::max(end);
-		// std::println("Highest {} from range {:}", mid, batteries);
-		// std::println();
+			mid = mid * 10 + *first; // + std::ranges::max(end);
+			first++;
+		}
 
+		// std::println("Highest number: {}", mid);
+		std::println();
 		sum += mid;
 	}
 
