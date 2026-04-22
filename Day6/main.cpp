193e0a1e70c14f0784ecfac45b37f1d4ea0812d6
Filip Lobpreis <f.lobpreis@gmail.com>
2026-04-22 21:21:20 +0200

    day6 pt 2
    Signed-off-by: Filip Lobpreis <f.lobpreis@gmail.com>

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -1,8 +1,11 @@
-#include "Day6.h"
+#include <algorithm>
+#include <fstream>
 #include <functional>
+#include <iterator>
 #include <numeric>
 #include <ranges>
 #include <print>
+#include <vector>
 
 using ull = unsigned long long;
 std::vector<std::string> split(const std::string &str, const char delimiter);
@@ -36,7 +39,7 @@ struct std::formatter<Calculation> {
     }
 };
 
-int main(int argc, char *argv[])
+void pt1()
 {
 	std::fstream input("../input", std::ios::in);
 	std::string line;
@@ -69,7 +72,65 @@ int main(int argc, char *argv[])
 	}
 
 	unsigned long long sum = 0;
-	std::ranges::for_each(calcs, [&sum](const Calculation &c) { std::println("{} returns: {}", c, c.execute()); sum += c.execute(); });
+	// std::ranges::for_each(calcs, [&sum](const Calculation &c) { std::println("{} returns: {}", c, c.execute()); sum += c.execute(); });
+
+	std::println("Sum is {}", sum);
+}
+
+int main(int argc, char *argv[])
+{
+	std::fstream input("../input", std::ios::in);
+	std::string line;
+	std::vector<Calculation> calcs;
+	std::vector<std::string> tmp_lines;
+
+	int i = 0;
+	while (std::getline(input, line)) {
+		tmp_lines.push_back(line);
+	}
+
+	auto start = tmp_lines.back().begin();
+	auto numsStart = 0;
+
+	while (true) {
+		auto endIt = std::find_if(start + 1, tmp_lines.back().end(), [](const char &c) { return c != ' '; });
+		char operation = *start;
+
+		std::println("Operator at the end: {}", *endIt);
+		auto blockSize = std::distance(start, endIt) - 1;
+		start = endIt;
+
+		if (endIt == tmp_lines.back().end())
+			blockSize++;
+
+		std::println("Block size: {}", blockSize);
+		Calculation c{.operation = operation};
+		std::ranges::fill_n(std::back_inserter(c.numbers), blockSize, 0);
+
+		for (size_t j = 0; j < tmp_lines.size()-1; j++) {
+			for (size_t i = numsStart; i < numsStart + blockSize; i++) {
+				if (tmp_lines[j][i] != ' ') {
+					c.numbers[i - numsStart] = c.numbers[i - numsStart] * 10 + tmp_lines[j][i] - '0';
+				}
+			}
+		}
+
+		numsStart += blockSize + 1;
+		std::println("Moving the block start to: {}", numsStart);
+
+		std::println("nums in c: {}", c);
+		calcs.push_back(std::move(c));
+
+		if (endIt == tmp_lines.back().end()) {
+			break;
+		}
+
+	}
+
+	std::ranges::for_each(calcs, [](const Calculation &c) { std::println("{}", c); });
+
+	unsigned long long sum = 0;
+	std::ranges::for_each(calcs, [&sum](const Calculation &c) { auto e = c.execute(); std::println("{} returns: {}", c, e); sum += e; });
 
 	std::println("Sum is {}", sum);
 
